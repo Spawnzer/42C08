d89fedbd8bfeb6a80dd0663138d0d4130b903015
Alexandre Dubeau <adubeau@c2r4p5.42quebec.com>
2021-03-05 06:04:33 -0500

    h

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:56:35 by adubeau           #+#    #+#             */
-/*   Updated: 2021/03/04 13:44:00 by adubeau          ###   ########.fr       */
+/*   Updated: 2021/03/05 06:02:59 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,41 +18,36 @@ int					ft_strlen(char *str)
 	int i;
 
 	i = 0;
-	while (*str + i)
+	while (str(i))
 		i++;
 	return (i);
 }
 
-char				*ft_strcpy(char *s1, char *s2)
-{
-	int i;
-
-	i = 0;
-	while (s1[i])
-	{
-		s2[i] = s1[i];
-		i++;
-	}
-	s2[i] = '\0';
-	return (s2);
-}
-
 char				*ft_strdup(char *src)
 {
-	char *dest;
+	char*dest;
+	int	i;
 
 	dest = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
 	if (!dest)
 		return (NULL);
-	dest = ft_strcpy(src, dest);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
 
 struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	int			i;
-	t_stock_str	*str;
+	int				i;
+	t_stock_str		*str;
 
+	if (ac < 0)
+		ac = 0;
 	i = 0;
 	str = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
 	if (!str)
