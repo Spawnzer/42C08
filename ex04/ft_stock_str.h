8099a7fd327fd30ded37e045de6e24d8ffe783a7
Alexandre Dubeau <adubeau@c2r4p5.42quebec.com>
2021-03-05 06:03:48 -0500

    j

--- ex04/ft_stock_str.h
+++ ex04/ft_stock_str.h
@@ -6,13 +6,18 @@
 /*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 15:04:40 by adubeau           #+#    #+#             */
-/*   Updated: 2021/03/03 15:05:12 by adubeau          ###   ########.fr       */
+/*   Updated: 2021/03/04 08:32:43 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct	s_stock_str
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef	struct	s_stock_str
 {
-	int size;
-	char *str;
-	char *copy;
+	int		size;
+	char	*str;
+	char	*copy;
 }				t_stock_str;
+
+#endif
